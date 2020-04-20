4b3c34767cf4179ce6e6657e1e1efa4f16b71682
leejinus <leejinus@gmail.com>
2020-04-20 17:41:28 +0900

    add FragmentChange

--- app/src/main/java/com/example/fragmenttest/MainFragment.java
+++ app/src/main/java/com/example/fragmenttest/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.fragmenttest;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,15 +10,41 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class MainFragment extends Fragment {
+    MainActivity activity;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        activity = (MainActivity) getActivity();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        activity = null;
+    }
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main,container,false);
+
+        Button button = (Button) rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activity.onFragmentChange(1);
+            }
+        });
+
+
+
         return rootView;
         //rootView에 레이아웃 불러와서 뿌려주기 (생명주기 관리)
     }
--- app/src/main/java/com/example/fragmenttest/MenuFragment.java
+++ app/src/main/java/com/example/fragmenttest/MenuFragment.java
@@ -1,9 +1,11 @@
 package com.example.fragmenttest;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,10 +14,34 @@ import androidx.fragment.app.Fragment;
 
 public class MenuFragment extends Fragment {
 
+    MainActivity activity;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        activity = (MainActivity) getActivity();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        activity = null;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_menu,container,false);
+
+        Button button2 = (Button) rootView.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activity.onFragmentChange(0);
+            }
+        });
+
+
         return rootView;
         //rootView에 레이아웃 불러와서 뿌려주기 (생명주기 관리)
     }
